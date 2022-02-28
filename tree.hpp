06a89778e8fdd293846c668ca0975cf3a958eaf3
shlu <shuyenlu@gmail.com>
2022-02-28 19:41:05 +0100

    treeee

--- tree.hpp
+++ tree.hpp
@@ -26,16 +26,55 @@ namespace ft {
 			nodePtr		_root;
 			nodePtr		_TNULL;
 
-			void		leftRotate(node n);
-			void		rightRotate(node n);
-			void		insertFix(node n);
-			void		deleteFix(node n);
+			nodePtr		newNode(T data)
+			{
+				nodePtr		n = new node;
+				n->data = data;
+				n->parent = nullptr;
+				n->left = nullptr;
+				n->right = nullptr;
+				n->color = 1;
+			}
+
+			nodePtr		move(nodePtr n, nodePtr *y, nodePtr x)
+			{
+				while (x != _TNULL)
+				{
+					*y = x;
+					if (n->data < x->data)
+						x = x->left;
+					else
+						x = x->right;
+				}
+				return x;
+			}
+			void		leftRotate(nodePtr n);
+			void		rightRotate(nodePtr n);
+			void		insertFix(nodePtr n);
+			void		deleteFix(nodePtr n);
 
 
 		public:
 
-			void		insertONode(node n);
-			void		deletionNode(node n);
+			RedBlackTree() {
+				_TNULL = new node;
+				_TNULL->color = 0;
+				_TNULL->left = nullptr;
+				_TNULL->right = nullptr;
+				_root = _TNULL;
+			}
+
+			void		insert(T data)
+			{
+				nodePtr n = newNode(data);
+				nodePtr y = nullPtr;
+				nodePtr x = move(n, &y, _root);
+				
+				
+				
+			}
+
+			void		deleteNode(T data);
 			nodePtr		search(T data);
 			nodePtr		getRoot() { return _root; }
 			void		printTree();
