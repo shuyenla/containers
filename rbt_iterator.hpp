3fd62326b16ed1d6e4002ea28492f8ede64bed75
shlu <shuyenlu@gmail.com>
2022-03-01 22:47:29 +0100

    rbt_iterator

--- rbt_iterator.hpp
+++ rbt_iterator.hpp
@@ -3,18 +3,35 @@
 
 namespace ft {
 
-	template<typename T>
+	template<typename N>
 	struct rbt_iterator
 	{
 		typedef bidirectional_iterator_tag 				iterator_category;
-		typedef T           							value_type;
+		typedef N           							value_type;
     	typedef ptrdiff_t    							difference_type;
-    	typedef T*		     							pointer;
-    	typedef T&			   							reference;
-		typedef rbt_iterator<T>							iterator;
-		typedef node<T>*								nodePtr;
+    	typedef N*		     							pointer;
+    	typedef N&			   							reference;
+		typedef rbt_iterator<N>							iterator;
+		typedef node<N>*								nodePtr;
 
-		nodePtr		_ptr;
+		nodePtr		_it;
+
+		rbt_iterator():_it(NULL) {}
+		explicit rbt_iterator(nodePtr N):_it(N) {}
+		
+
+		reference operator*() const { return *_it; }
+    	pointer operator->() const { return _it; }
+    	iterator& operator++() { _it++; return *this; }
+    	iterator  operator++(int) {
+			iterator it = *this;
+			++(*this);
+			return it; }
+    	iterator& operator--() { _it--; return *this; }
+    	iterator  operator--(int) {
+			iterator it = *this;
+			--(*this);
+			return it; }
 
 
 	};
